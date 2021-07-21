d526bdf4041c4e7c5946a3bb78867edcbb7e09b9
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-21 01:08:29 -0300

    feat: create splashscreen

---
+++ src/screens/SplashScreen/index.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { CustomText, Logo } from "../../components"
+
+export const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+        <Logo />
+        <CustomText color="">StarWars - Wiki</CustomText>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#161616',
+    alignItems: 'center',
+    justifyContent: 'center',
+    },
+});
